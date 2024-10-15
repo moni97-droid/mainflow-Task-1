31665eac15ee5a687fcc94cfac80f8e7a051f600
moni97-droid <monistar16@gmail.com>
2024-10-15 19:24:58 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,53 @@
+#Creating List
+my_list=[1,2,3,4,5]
+
+#Adding
+my_list.append(6)
+
+#Removing
+my_list.remove(3)
+
+#Modifying
+my_list[0]=10
+
+print ("Updated List :"  , my_list)
+
+
+
+
+
+
+#Creating a dictionary
+my_dict = {'name':'Esha','age':26,'city':'Mumbai'}
+
+#Adding
+my_dict['gender']='Female'
+
+#Removing
+del my_dict['age']
+
+#Modifying
+my_dict ['City']='Kolkata'
+
+print("Updated Dictionary:" , my_dict)
+
+
+
+
+#Creating a set
+my_set={1,2,3,4,5}
+
+
+#Adding
+my_set.add(6)
+
+#Removing
+my_set.remove(4)
+
+#Modifying
+my_set.discard(1)
+my_set.add(10)
+
+print("Updated set:" , my_set)
+
+
